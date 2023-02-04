fcd647a90b0741ef5be3802ade6edbdcf6ee6245
cbarnes0 <carsonbarnes98@gmail.com>
2023-02-03 20:32:16 -0500

    updated code, working on functionality of JS. got to a good point to update

--- Development/assets/script.js
+++ Development/assets/script.js
@@ -1,9 +1,66 @@
 var apiKey = "e90ad556cc55926905eb32cc8f08f4f3";
 var city = "Atlanta";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
 var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
 
-var data = fetch(geoCode);
+var savedSearches = [];
 
-console.log(data);
+// var date
+// var weatherIcon
+var currentTemp = $("#temp");
+var currentHumid = $("#humid");
+var currentWSpeed = $("#wind");
+var cityName = $("#cityname")
+
+// how to get info from search bar
+
+
+// Function to get weather data from api after getting coords
+function getWeatherdata() {
+fetch(geoCode, {
+    method: 'GET',
+})
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    
+    latitude = data[0].lat;
+    longitude = data[0].lon;
+    
+    return latitude, longitude;
+  })
+  .then(function() {
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+
+    fetch(queryURL, {
+        method: 'GET',
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        showWeatherdata(data);
+    })
+  });
+};
+  getWeatherdata();
+
+function showWeatherdata(data) {
+    var todayDate = data.list[0].dt_txt;
+    var weatherIcon = data.list[0].weather[0].icon;
+    var weatherIconImg = $("<img>");
+    weatherIconImg.attr("src", `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`);
+    console.log(todayDate);
+    var todayTemp = data.list[0].main.temp;
+    var todayWind = data.list[0].wind.speed;
+    var todayHumid = data.list[0].main.humidity;
+
+    cityName.append(todayDate);
+    cityName.append(weatherIconImg);
+
+    currentTemp.text(todayTemp + " \u00B0F");
+    currentWSpeed.text(todayWind + " MPH");
+    currentHumid.text(todayHumid + "%");
+}
